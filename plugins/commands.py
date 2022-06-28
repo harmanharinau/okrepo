59c586e09d5e78cabd8bb72ac21dec676d900ecc
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-06-28 16:56:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,7 +38,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [
+        buttons = [[
             InlineKeyboardButton('🎁ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ sᴜʀᴘʀɪsᴇ🎁', callback_data='start')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
